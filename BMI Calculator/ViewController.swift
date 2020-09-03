a44d265a00c7c03543312e270286b94b8a5d3e2d
الدانة سعد الرشيدي-61 <66528015+Aldanaalrashidi@users.noreply.github.com>
2020-09-03 20:32:06 +0300

    Done

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var calculateBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculateBtn.layer.cornerRadius = 30
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func isTapped(_ sender: Any) {
+        calculateBmiAndUpdateView()
+    }
+    
+    func calculateBmi(weight: Double, height: Double) -> Double{
+          return weight / (height*height)
+      }
+      
+      func bmiStatus(bmi: Double) -> String {
+          if bmi < 20{
+              return "ضعيف"
+          }else if bmi < 25 {
+              return "جيد"
+          }else if bmi > 25{
+              return "سمين"
+          }
+          else{
+            return "خطأ"
+        }
+      }
+    
+    func calculateBmiAndUpdateView(){
+        let weight = Double(weightField.text!)
+        let height = Double(heightField.text!)
+        let bmi = calculateBmi(weight: weight ?? 0.0, height: height ?? 0.0)
+        let bmiStatus = self.bmiStatus(bmi: bmi)
+        self.bmiLabel.text = String(format: "%.1f", bmi)
+        self.statusLabel.text = bmiStatus
     }
 }
 
