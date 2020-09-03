ac6d38f5d9826fa91807628b4654e137480859c1
الدانة سعد الرشيدي-61 <66528015+Aldanaalrashidi@users.noreply.github.com>
2020-09-03 18:18:46 +0300

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Aldana Al Rashidi on 9/3/20.
+//  Copyright © 2020 Aldana Al Rashidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
